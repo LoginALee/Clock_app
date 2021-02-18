8c3b4111f5324537f2c45ff512e4d079f594a300
Jorge Vargas <jvargas18@ucol.mx>
2021-02-18 02:21:45 -0600

    Add new time_zone field to User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_194925) do
+ActiveRecord::Schema.define(version: 2021_02_18_024320) do
 
   create_table "alarms", force: :cascade do |t|
     t.datetime "date_to_sound"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_194925) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "time_zone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
