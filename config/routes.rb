6283920391824d5a2556791abe94e5394056a2d0
Jorge Vargas <jvargas18@ucol.mx>
2021-02-18 02:26:24 -0600

    Add method to change current time zone

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "main#home"
+  root 'main#home'
   resources :alarms
   resources :stopwatches
   resources :timezones
+  get 'users/:id/edit_time_zone', to: 'users#edit_time_zone', as: :edit_time_zone
+  put 'users/:id/edit_time_zone', to: 'users#update_time_zone', as: :update_time_zone
+  patch 'users/:id/edit_time_zone', to: 'users#update_time_zone'
 end
