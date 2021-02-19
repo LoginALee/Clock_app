e78d4c4e42d448628c785656d9b79b4e95294ca6
Jorge Vargas <jvargas18@ucol.mx>
2021-02-19 11:00:01 -0600

    Add methods and views for stopwatches

--- app/controllers/stopwatches_controller.rb
+++ app/controllers/stopwatches_controller.rb
@@ -1,9 +1,37 @@
 class StopwatchesController < ApplicationController
+  before_action :set_stopwatch, only: %i[destroy]
+
+  def index
+    @stopwatch = current_user.stopwatches.build
+    @stopwatches = current_user.stopwatches.all
+  end
+
   def new
-    @stopwatch = Stopwatch.new 
+    @stopwatch = current_user.stopwatches.build
   end
 
   def create
+    @stopwatch = current_user.stopwatches.build(stopwatch_params)
 
+    if @stopwatch.save
+      redirect_to stopwatches_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @stopwatch.destroy
+    redirect_to stopwatches_path
+  end
+
+  private
+
+  def set_stopwatch
+    @stopwatch = current_user.stopwatches.find(params[:id])
+  end
+
+  def stopwatch_params
+    params.require(:stopwatch).permit(:time)
   end
 end
