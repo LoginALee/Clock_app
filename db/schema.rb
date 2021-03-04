a350447fe381751c33204438a43f5ec3ea996466
Jorge Vargas <jvargas18@ucol.mx>
2021-03-04 02:37:19 -0600

    Add name field for Stopwatches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_193738) do
+ActiveRecord::Schema.define(version: 2021_03_04_060613) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_193738) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
     t.index ["user_id"], name: "index_stopwatches_on_user_id"
   end
 
