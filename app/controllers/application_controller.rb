d458a59acf7e543d656aca8b6c35b2ef07d1e380
Jorge Vargas <jvargas18@ucol.mx>
2021-03-22 11:50:09 -0600

    Add simplecov filters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,2 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-
-  def unprocessable_entity(exception)
-    render plain: "Unprocessable Entity #{exception.record.errors}", status: :unprocessable_entity
-  end
-
-  def record_not_found(exception)
-    render plain: "404 Not Found #{exception.message}", status: :not_found
-  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,12 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 require 'simplecov'
 
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter '/bin/'
+  add_filter '/channels/'
+  add_filter '/db/'
+  add_filter '/spec/'
+end
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
