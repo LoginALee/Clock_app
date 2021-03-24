c54e969ca89eadd89c6e5bc938b60e35877c1b46
Jorge Vargas <jvargas18@ucol.mx>
2021-03-23 19:06:13 -0600

    Add avatar image to User factory

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     email { Faker::Internet.email }
     password { Faker::Internet.password }
+    after(:build) do |user|
+      user.avatar.attach(io: File.open('spec/fixtures/perfil.jpeg'), filename: 'perfil.jpeg',
+                         content_type: 'image/jpeg')
+    end
   end
 end
