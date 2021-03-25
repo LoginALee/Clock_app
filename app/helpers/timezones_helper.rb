260f381c70643a27f68694a3aaaa6f2aa6e46361
Jorge Vargas <jvargas18@ucol.mx>
2021-03-25 17:48:14 -0600

    Change timezone helper to return a sring

--- app/helpers/timezones_helper.rb
+++ app/helpers/timezones_helper.rb
@@ -1,9 +1,9 @@
 module TimezonesHelper
   def time_zone_format(time_zone)
-    ActiveSupport::TimeZone[time_zone]
+    ActiveSupport::TimeZone[time_zone].to_s
   end
 
   def time_zone_info(time_zone)
-    ActiveSupport::TimeZone[time_zone].now
+    ActiveSupport::TimeZone[time_zone].now.to_s
   end
 end
